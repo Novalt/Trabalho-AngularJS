f40244a185a5b0d0ae6d17fcc6fd93b2f5eec8f8
Novalt Corporation <rodinsarmento8@gmail.com>
2023-10-08 22:52:33 -0300

    Update app.js

--- data-biding-prototipo/app.js
+++ data-biding-prototipo/app.js
@@ -1,6 +1,5 @@
 angular.module('dataBindingApp', [])
 .controller('ExerciciosController', function($scope) {
-  
   $scope.helloMessage = "Olá mundo";
 
   $scope.link = "https://www.example.com";
@@ -20,12 +19,12 @@ angular.module('dataBindingApp', [])
 })
 .component('exercicio', {
   template: `
+    <a ng-href="{{link}}">Link</a>
     <p>{{helloMessage}}</p>
     <p ng-click="toggleParagrafo()" ng-style="{ 'background-color': paragrafoClicado ? 'red' : 'white', 'color': paragrafoClicado ? 'white' : 'black' }">Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae. </p>
     <p ng-click="toggleParagrafo()" ng-style="{ 'background-color': paragrafoClicado ? 'yellow' : 'white', 'color': paragrafoClicado ? 'white' : 'black' }">Donec vulputate justo a scelerisque scelerisque. Phasellus fringilla enim velit. </p>
     <p ng-click="toggleParagrafo()" ng-style="{ 'background-color': paragrafoClicado ? 'green' : 'white', 'color': paragrafoClicado ? 'white' : 'black' }">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in est vel tellus tincidunt Integer.</p>
     <p ng-click="toggleParagrafo()" ng-style="{ 'background-color': paragrafoClicado ? 'purple' : 'white', 'color': paragrafoClicado ? 'white' : 'black' }">sit amet pellentesque velit posuere condimentum. Nulla pellentesque ornare rutrum. Nunc magna dui.</p>
-    <a ng-href="{{link}}">Link</a>
     <select ng-model="selecaoTextos" ng-options="opcao for opcao in textos"></select>
     <p ng-show="selecaoTextos !== ''">{{textos[selecaoTextos]}}</p>
   `,
